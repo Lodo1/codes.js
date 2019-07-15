c2a580db00ad69ea4484cddcbae3fd56f1cb8a29
Dareen Moughrabi <dareenmoughrabi@gmail.com>
2019-07-15 12:39:26 +0300

    saved the files 1 time

---
+++ function.js
@@ -0,0 +1,19 @@
+ function max(a,b) {
+ 	if (a>b) {
+ 	return a; }
+ 		else{
+ 			return b;
+ 		}
+ }
+ var c = max(1,4);
+
+ alert(c);
+ var name = prompt(" what is your name");
+alert(name);
+var party= confirm( "are you coming to my birthday party ?");
+if (party == true ){
+	alert( " i cant wait to see you  , i am really happy ");
+}
+else  {
+	alert(" oh  wr=e will miss you very much ");
+}
